2133deb4cecfd3e01dca60d557beee9cce3c9e0a
Karlo Ciciliani <karlo.ciciliani1@gmail.com>
2026-04-09 10:45:36 +0200

    feat: add program leaderboard, fix class scope, auto-populate StudentProgram

---
+++ Migrations/20260409083004_AddLeaderboardStudentProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_Dnevnik_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLeaderboardStudentProgram : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StudentProgram",
+                table: "LeaderboardEntries",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentProgram",
+                table: "LeaderboardEntries");
+        }
+    }
+}
